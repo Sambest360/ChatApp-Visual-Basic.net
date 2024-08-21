3e5c4d99321fe28760604755706b23dbaacc3b2a
Sambest360 <sambampo200@gmail.com>
2024-08-21 21:40:27 +0200

    update

--- Client/Client/Form1.vb
+++ Client/Client/Form1.vb
@@ -5,13 +5,17 @@ Imports System.Text
 Public Class Form1
 
     Dim client As TcpClient
+    Dim username As String
 
     Private Sub btnConnect_Click(sender As Object, e As EventArgs) Handles btnConnect.Click
         Try
-            Dim ip As String = "192.168.1.105"
+            Dim ip As String = "10.16.170.120"
             Dim port As Integer = 6655
 
             client = New TcpClient(ip, port)
+
+            ' Prompt the user to enter a username
+            username = InputBox("Enter your username:", "Username")
 
             CheckForIllegalCrossThreadCalls = False
 
@@ -32,17 +36,14 @@ Public Class Form1
                 Dim ns As NetworkStream = client.GetStream()
 
                 Dim toRecive(100000) As Byte
-                ns.Read(toRecive, 0, toRecive.Length)
-                Dim txt As String = Encoding.ASCII.GetString(toRecive)
-
+                Dim bytesRead As Integer = ns.Read(toRecive, 0, toRecive.Length)
+                Dim txt As String = Encoding.ASCII.GetString(toRecive, 0, bytesRead)
 
                 If RichTextBox1.Text.Length > 0 Then
-
                     RichTextBox1.Text &= vbNewLine & txt
                 Else
                     RichTextBox1.Text = txt
                 End If
-
 
             End While
         Catch ex As Exception
@@ -52,10 +53,18 @@ Public Class Form1
 
     Private Sub btnSned_Click(sender As Object, e As EventArgs) Handles btnSned.Click
         Try
-
             Dim ns As NetworkStream = client.GetStream()
 
-            ns.Write(Encoding.ASCII.GetBytes(TextBox1.Text), 0, TextBox1.Text.Length)
+            ' Send the message with the username
+            Dim message As String = username & ": " & TextBox1.Text
+            ns.Write(Encoding.ASCII.GetBytes(message), 0, message.Length)
+
+            ' Display the sent message in the chat box
+            If RichTextBox1.Text.Length > 0 Then
+                RichTextBox1.Text &= vbNewLine & message
+            Else
+                RichTextBox1.Text = message
+            End If
 
         Catch ex As Exception
             MsgBox(ex.Message)
--- Server/Server/Form1.vb
+++ Server/Server/Form1.vb
@@ -2,7 +2,6 @@
 Imports System.Net.Sockets
 Imports System.Text
 
-
 Public Class Form1
 
     Dim server As TcpListener
@@ -10,13 +9,13 @@ Public Class Form1
 
     Private Sub btnStartServer_Click(sender As Object, e As EventArgs) Handles btnStartServer.Click
         Try
-            Dim ip As String = "192.168.1.106"
+            Dim ip As String = "10.16.170.120"
             Dim port As Integer = 6655
 
             server = New TcpListener(IPAddress.Parse(ip), port)
             server.Start()
 
-            Threading.ThreadPool.QueueUserWorkItem(AddressOf NewClient)
+            Threading.ThreadPool.QueueUserWorkItem(AddressOf newClient)
 
         Catch ex As Exception
             MsgBox(ex.Message)
@@ -33,14 +32,12 @@ Public Class Form1
             While True
                 Dim ns As NetworkStream = client.GetStream()
                 Dim toReceive(100000) As Byte
-                ns.Read(toReceive, 0, toReceive.Length)
-                Dim txt As String = Encoding.ASCII.GetString(toReceive)
+                Dim bytesRead As Integer = ns.Read(toReceive, 0, toReceive.Length)
+                Dim txt As String = Encoding.ASCII.GetString(toReceive, 0, bytesRead)
 
                 For Each c As TcpClient In listOfClients
-                    If c IsNot client Then
-                        Dim nns As NetworkStream = c.GetStream()
-                        nns.Write(Encoding.ASCII.GetBytes(txt), 0, txt.Length)
-                    End If
+                    Dim nns As NetworkStream = c.GetStream()
+                    nns.Write(Encoding.ASCII.GetBytes(txt), 0, txt.Length)
                 Next
             End While
 
